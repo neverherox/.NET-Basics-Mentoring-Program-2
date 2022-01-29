579ada4f7fe31fad604c271b947a6bca1a66ba62
Kiryl_Ramashkevich <Kiryl_Ramashkevich@epam.com>
2022-01-29 16:48:46 +0300

    Unit testing task add Calculate method realisation

--- UnitTesting/FizzBuzz/FizzBuzz.cs
+++ UnitTesting/FizzBuzz/FizzBuzz.cs
@@ -6,7 +6,23 @@ namespace FB
     {
         public string Calculate(int number)
         {
-            throw new NotImplementedException();
+            if (number < 1 || number > 100)
+            {
+                throw new ArgumentOutOfRangeException(nameof(number));
+            }
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            return number.ToString();
         }
     }
 }
