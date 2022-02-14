fd5bbab7f88758e269784eb9d77b906c82f40e35
Kiryl_Ramashkevich <Kiryl_Ramashkevich@epam.com>
2022-02-14 17:04:29 +0300

    Logging task log4net logger added

---
+++ Logging/BrainstormSessions/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using BrainstormSessions.Core.Interfaces;
+using BrainstormSessions.Core.Model;
+using BrainstormSessions.ViewModels;
+using log4net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrainstormSessions.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IBrainstormSessionRepository _sessionRepository;
+        private readonly ILog _logger = LogManager.GetLogger(typeof(HomeController));
+
+        public HomeController(
+            IBrainstormSessionRepository sessionRepository)
+        {
+            _sessionRepository = sessionRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            _logger.Info($"{nameof(Index)} starting request");
+
+            var sessionList = await _sessionRepository.ListAsync();
+
+            var model = sessionList.Select(session => new StormSessionViewModel()
+            {
+                Id = session.Id,
+                DateCreated = session.DateCreated,
+                Name = session.Name,
+                IdeaCount = session.Ideas.Count
+            });
+
+            return View(model);
+        }
+
+        public class NewSessionModel
+        {
+            [Required]
+            public string SessionName { get; set; }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(NewSessionModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                _logger.Warn($"{nameof(Index)} model state is invalid");
+
+                return BadRequest(ModelState);
+            }
+            else
+            {
+                await _sessionRepository.AddAsync(new BrainstormSession()
+                {
+                    DateCreated = DateTimeOffset.Now,
+                    Name = model.SessionName
+                });
+            }
+
+            return RedirectToAction(actionName: nameof(Index));
+        }
+    }
+}
---
+++ Logging/BrainstormSessions/Controllers/SessionController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using BrainstormSessions.Core.Interfaces;
+using BrainstormSessions.ViewModels;
+using log4net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BrainstormSessions.Controllers
+{
+    public class SessionController : Controller
+    {
+        private readonly IBrainstormSessionRepository _sessionRepository;
+        private readonly ILog _logger = LogManager.GetLogger(typeof(HomeController));
+
+        public SessionController(
+            IBrainstormSessionRepository sessionRepository)
+        {
+            _sessionRepository = sessionRepository;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            _logger.Debug($"{nameof(Index)} starting request");
+
+            if (!id.HasValue)
+            {
+                return RedirectToAction(actionName: nameof(Index),
+                    controllerName: "Home");
+            }
+
+            var session = await _sessionRepository.GetByIdAsync(id.Value);
+
+            if (session == null)
+            {
+                return Content("Session not found.");
+            }
+
+            _logger.Debug($"{nameof(Index)} session id {session.Id}");
+
+            var viewModel = new StormSessionViewModel()
+            {
+                DateCreated = session.DateCreated,
+                Name = session.Name,
+                Id = session.Id
+            };
+
+            return View(viewModel);
+        }
+    }
+}
