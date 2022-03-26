60b261edefd10fe880d6d4f7d8787b685fde545f
Kiryl_Ramashkevich <Kiryl_Ramashkevich@epam.com>
2022-03-26 19:01:45 +0300

    Http task implement

---
+++ Http/HttpClient/HttpClient/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var cookies = new CookieContainer();
+            var handler = new HttpClientHandler
+            {
+                CookieContainer = cookies
+            };
+            var client = new HttpClient(handler);
+
+            var nameResponse = await client.GetAsync("http://localhost:8888/MyName/");
+            var name = await nameResponse.Content.ReadAsStringAsync();
+            Console.WriteLine(name);
+
+            var infoResponse = await client.GetAsync("http://localhost:8888/Information/");
+            var infoStatusCode = infoResponse.StatusCode.ToString();
+            Console.WriteLine(infoStatusCode);
+
+            var successResponse = await client.GetAsync("http://localhost:8888/Success/");
+            var successStatusCode = successResponse.StatusCode.ToString();
+            Console.WriteLine(successStatusCode);
+
+            var redirectResponse = await client.GetAsync("http://localhost:8888/Redirection/");
+            var redirectStatusCode = redirectResponse.StatusCode.ToString();
+            Console.WriteLine(redirectStatusCode);
+
+            var clientErrorResponse = await client.GetAsync("http://localhost:8888/ClientError/");
+            var clientErrorStatusCode = clientErrorResponse.StatusCode.ToString();
+            Console.WriteLine(clientErrorStatusCode);
+
+            var serverErrorResponse = await client.GetAsync("http://localhost:8888/ServerError/");
+            var serverErrorStatusCode = serverErrorResponse.StatusCode.ToString();
+            Console.WriteLine(serverErrorStatusCode);
+
+            var headerNameResponse = await client.GetAsync("http://localhost:8888/MyNameByHeader/");
+            var headerName = headerNameResponse.Headers.GetValues("X-MyName").First();
+            Console.WriteLine(headerName);
+
+            var cookieNameResponse = await client.GetAsync("http://localhost:8888/MyNameByCookies/");
+            var cookieName = cookies.GetCookies(new Uri("http://localhost:8888/MyNameByCookies/"))
+                .First(x => x.Name == "MyName").Value;
+            Console.WriteLine(cookieName);
+        }
+    }
+}
---
+++ Http/HttpListener/HttpListener/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace Listener
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost:8888/");
+
+            while (true)
+            {
+                listener.Start();
+                var context = listener.GetContext();
+                var request = context.Request;
+                using var response = context.Response;
+                var method = GetMethod(request);
+                if (method == "MyName")
+                {
+                    GetMyName(response);
+                }
+                if (method == "Information")
+                {
+                    response.StatusCode = (int)HttpStatusCode.SwitchingProtocols;
+                    response.KeepAlive = false;
+                    GetMyName(response);
+                }
+                if (method == "Success")
+                {
+                    response.StatusCode = (int)HttpStatusCode.OK;
+                    GetMyName(response);
+                }
+                if (method == "Redirection")
+                {
+                    response.StatusCode = (int)HttpStatusCode.Redirect;
+                    GetMyName(response);
+                }
+                if (method == "ClientError")
+                {
+                    response.StatusCode = (int)HttpStatusCode.NotFound;
+                    GetMyName(response);
+                }
+                if (method == "ServerError")
+                {
+                    response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                    GetMyName(response);
+                }
+                if (method == "MyNameByHeader")
+                {
+                    GetMyNameByHeader(response);
+                }
+                if (method == "MyNameByCookies")
+                {
+                    MyNameByCookies(response);
+                }
+                listener.Stop();
+            }
+        }
+
+        static string GetMethod(HttpListenerRequest request)
+        {
+            var path = request.Url.AbsolutePath;
+            var method = path
+                .Split("/")
+                .First(x => !string.IsNullOrEmpty(x));
+            return method;
+        }
+
+        static void GetMyName(HttpListenerResponse response)
+        {
+            using var output = response.OutputStream;
+            var buffer = Encoding.UTF8.GetBytes("Kiryl");
+            response.ContentLength64 = buffer.Length;
+            output.Write(buffer, 0, buffer.Length);
+        }
+
+        static void GetMyNameByHeader(HttpListenerResponse response)
+        {
+            response.AddHeader("X-MyName", "Kiryl");
+            GetMyName(response);
+        }
+
+        static void MyNameByCookies(HttpListenerResponse response)
+        {
+            response.Cookies.Add(new Cookie
+            {
+                Name = "MyName",
+                Value = "Kiryl"
+            });
+            GetMyName(response);
+        }
+    }
+}
